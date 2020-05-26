24cb8ea627f8a49a4edf36ae99d070571d444636
Joshua Fernandes <joshua.fernandes@consensys.net>
2020-05-26 13:52:37 +1000

    fixing filter

--- filter_plugins/besu_filters.py
+++ filter_plugins/besu_filters.py
@@ -3,16 +3,17 @@
 from typing import Union, List
 
 
+def besu_opts_format(besu_env_opts: Union[str, List[str]]):
+    if isinstance(besu_env_opts, str):
+        besu_env_opts = besu_env_opts.split()
+
+    escaped_besu_env_opts = [item.replace('"', '\\"') for item in besu_env_opts]
+
+    return f'''"{'" "'.join(escaped_besu_env_opts)}"'''
+
+
 class FilterModule(object):
     def filters(self):
         return {
-            'besu_opts_format': self.besuOptsFormat,
+            'besu_opts_format': besu_opts_format,
         }
-
-    def besuOptsFormat(self, besu_env_opts: Union[str, List[str]]):
-        if isinstance(besu_env_opts, str):
-            besu_env_opts = besu_env_opts.split()
-
-        escaped_besu_env_opts = [item.replace('"', '\\"') for item in besu_env_opts]
-
-        return f'''"{'" "'.join(escaped_besu_env_opts)}"'''
